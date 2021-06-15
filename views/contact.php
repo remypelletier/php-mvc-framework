8e3d76c5931ff5eec3839dc6e3b21c541060a8a4
Rémy Pelletier <remypelletier2@gmail.com>
2021-06-15 11:26:01 +0200

    add: basic style
    closes #4

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,20 @@
 <h1>contact</h1>
+
+<form class="form" method="post">
+    <fieldset class="form-fields">
+        <legend class="form-legend">Write us using this form</legend>
+        <p class="form-field">
+            <label class="form-field__label" for="subject">Subject</label>
+            <input class="form-field__input" type="text" name="subject" id="subject">
+        </p>
+        <p class="form-field">
+            <label class="form-field__label" for="email">Email</label>
+            <input class="form-field__input" type="email" name="email" id="email">
+        </p>
+        <p class="form-field">
+            <label class="form-field__label" for="message">Message</label>
+            <textarea class="form-field__textarea" name="message" id="message"></textarea>
+        </p>
+        <button class="form-field__button" type="submit">Send</button>
+    </fieldset>
+</form>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,18 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="/build/img/favicon.png" />
+    <link rel="stylesheet" href="/build/css/style.css">
     <title>main layout</title>
 </head>
 <body>
-    <head>
-        <a href="/">Php MVC framework</a>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </head>
-    <main>
-    {{content}}
-    </main>
+    <header class="header">
+        <a href="/" class="header__logo">Framework</a>
+        <nav>
+            <div class="header__menu-icon"><span class="navicon"></span></div>
+            <ul class="header__menu">
+                <li><a href="/">Home</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="main-container">
+        {{content}}
+    </section>
+    <script src="/build/js/script.js"></script>
 </body>
 </html>
